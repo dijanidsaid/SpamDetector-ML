cea6fde8db7689f45340eb895d17bda0be830a84
dijanidsaid <nidsaidkhadija88@gmail.com>
2025-03-06 22:13:58 +0100

    hope

--- app.py
+++ app.py
@@ -48,4 +48,5 @@ def predict_api():
 
 # Run the Flask app
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)  # Run on all available IPs, port 5000
+    #app.run(host="0.0.0.0", port=5000, debug=True)  # Run on all available IPs, port 5000
+    app.run()
